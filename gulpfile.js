6bb09b4d776e8da631fd1593cdf1bdef6f537bb6
taras <taraskalini@gmail.com>
2017-01-22 11:03:23 +0200

    sborka

--- gulpfile.js
+++ gulpfile.js
@@ -45,6 +45,8 @@ gulp.task('scripts', function() {
 		'./app/libs/jquery/jquery-1.11.2.min.js',
 		'./app/libs/fancyBox/jquery.fancybox.js',
 		'./app/libs/plugins-scroll/plugins-scroll.js',
+		'./app/libs/magnific-popup/dist/jquery.magnific-popup.min.js',
+		'./app/libs/lightslider/dist/js/lightslider.min.js',
 		])
 		.pipe(concat('libs.js'))
 		//.pipe(uglify()) //Minify libs.js
